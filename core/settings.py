02072c778f93ac6beb6e59c4f1c0bf93eb16fdd0
Asalgrs24 <gharibshahgr@yahoo.com>
2024-04-07 12:12:15 +0330

    build up the dashboard page

--- core/settings.py
+++ core/settings.py
@@ -101,3 +101,6 @@ BASKET_SESSION_ID = 'basket'
 AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
